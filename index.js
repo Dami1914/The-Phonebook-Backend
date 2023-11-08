082f4668a46a26de9a6e712c3e7d2c025c7aec59
Damilola Jibowu <Aesthetico.design@gmail.com>
2023-11-08 02:27:23 +0100

    build:ui

--- index.js
+++ index.js
@@ -1,137 +1,145 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/peopleContact')
-const app = express()
+/* eslint-disable linebreak-style */
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/peopleContact');
 
-app.use(express.json())
+const app = express();
 
-morgan.token('body',(req,res)=>{
-    return JSON.stringify(req.body)
-})
+app.use(express.json());
 
-app.use(cors())
-app.use(express.static('dist'))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+app.use(cors());
+app.use(express.static('dist'));
 
-app.use(morgan(':method :url :status :response-time ms - :body'))
+app.use(morgan(':method :url :status :response-time ms - :body'));
 
-app.use((request,response,next)=>{
-  request.requestDate = new Date().toUTCString()
-  next()
-})
+app.use((request, response, next) => {
+  request.requestDate = new Date().toUTCString();
+  next();
+});
 
-app.put('/api/persons/:id',(request,response,next)=>{
-      const body = request.body
-      const person = {
-        name: body.name,
-        number: body.number
-      }
-      Person.findByIdAndUpdate(request.params.id,person,{new:true, runValidators: true, contex:"query"})
-      .then((result)=>{
-        response.send(result)
-        console.log("Data updated successfully",result)
-      })
-      .catch((error)=>{
-          next(error)
-      })
-
-})
-app.get('/api/persons/:id',(request,response,next)=>{
-    const reqId = request.params.id
-    Person.findById(reqId)
-    .then(dbRes=>{
-        if(dbRes.length === 0){
-          response.statusMessage = `the id ${reqId} not present`
-          response.status(404).end(`the id ${reqId} not present`)
-        }else{
-          response.send(dbRes)
-          console.log(dbRes)
-        }
-    })
-    .catch((error)=>{
-      next(error)
-    })
-})
-
-app.delete('/api/persons/:id',(request,response,next)=>{
-    Person.findByIdAndRemove(request.params.id)
-      .then((result)=>{
-          
-          response.send(result).end()
-          console.log(result,"data removed successfully")
-
-      }).catch((error)=>{
-         next(error)
-      })
-})
-app.post('/api/persons',(request,response,next)=>{
-
-    const body = request.body
-    
-    const person = new Person({
-      name: body.name,
-      number: body.number
-    })
-   
-    Person.find({name:body.name, number:body.number})
-    .then((result)=>{
-      
-      if(result.length > 0){
-        return response.status(400).json({
-          error: "name available"
-        })
-      }
-
-      person.save().then((result)=>{
-        response.send(person)
-      }).catch((err)=>{
-        next(err)
-      })
-
-  }).catch((err)=>{
-    next(err)
+app.put('/api/persons/:id', (request, response, next) => {
+  const { body } = request;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    contex: 'query',
   })
-    
-})
-app.get('/api/persons',(request,response,next)=>{
-    Person.find({}).then((data)=>{
-      response.json(data)
+    .then((result) => {
+      response.send(result);
+      console.log('Data updated successfully', result);
     })
-    .catch((err)=>{
-      next(err)
-    })
-})
+    .catch((error) => {
+      next(error);
+    });
+});
 
-app.get('/api/info',(request,response,next)=>{
-    Person.find({}).then((dbRes)=>{
-      response.send(`<div><p>Phonebook has info for ${dbRes.length} people</p><p>${request.requestDate}</p></div>`)
-    }).catch((error)=>{
-      next(error)
+app.get('/api/persons/:id', (request, response, next) => {
+  const reqId = request.params.id;
+  Person.findById(reqId)
+    .then((dbRes) => {
+      if (dbRes.length === 0) {
+        response.statusMessage = `the id ${reqId} not present`;
+        response.status(404).end(`the id ${reqId} not present`);
+      } else {
+        response.send(dbRes);
+        console.log(dbRes);
+      }
     })
-})
+    .catch((error) => {
+      next(error);
+    });
+});
 
-function unknownEndpoint(req,res){
-    res.status(404).send({message: "sorry the endpoint you requested does not exist"})
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.send(result).end();
+      console.log(result, 'data removed successfully');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+app.post('/api/persons', (request, response, next) => {
+  const { body } = request;
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  Person.find({ name: body.name, number: body.number })
+    .then((result) => {
+      if (result.length > 0) {
+        return response.status(400).json({
+          error: 'name available',
+        });
+      }
+
+      person
+        .save()
+        .then((result) => {
+          response.send(person);
+        })
+        .catch((err) => {
+          next(err);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then((data) => {
+      response.json(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.get('/api/info', (request, response, next) => {
+  Person.find({})
+    .then((dbRes) => {
+      response.send(
+        `<div><p>Phonebook has info for ${dbRes.length} people</p><p>${request.requestDate}</p></div>`,
+      );
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+function unknownEndpoint(req, res) {
+  res
+    .status(404)
+    .send({ message: 'sorry the endpoint you requested does not exist' });
 }
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-function errorHandler(error,request,response,next){
-  console.error(error)
+function errorHandler(error, request, response, next) {
+  console.error(error);
 
-  const statuscode = error.statusCode || 500
-  const errormessage = error.message || 'internal server error' 
+  const statuscode = error.statusCode || 500;
+  const errormessage = error.message || 'internal server error';
 
-  response.status(statuscode).send(errormessage)
-  
+  response.status(statuscode).send(errormessage);
 }
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3002;
 
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
--- models/peopleContact.js
+++ models/peopleContact.js
@@ -1,74 +1,62 @@
-const mongoose = require('mongoose')
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
 
-mongoose.set('strictQuery',false)
+const url = process.env.MONGODB_URL;
 
-
-const url = process.env.MONGODB_URL
-
-mongoose.connect(url)
-.then((result)=>{
-    console.log("connected to database successfully")
-})
-.catch((error)=>{
-    console.log("couldn't connect to the database")
-})
-
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to database successfully');
+  })
+  .catch((error) => {
+    console.log("couldn't connect to the database");
+  });
 
 const phonebookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate:{
-           validator:(value)=>{
-            const contact = value.split("-")
-            if(contact[0].length < 2 || contact[0].length > 3){
-                throw new Error("not a valid phone number")
-            }else{
-                
-                if( isNaN(Number(contact.join(""))) ){
-                    throw new Error("the value you provided is not a number ")
-                }
-            }
-            return true
-           }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: (value) => {
+        const contact = value.split('-');
+        if (contact[0].length < 2 || contact[0].length > 3) {
+          throw new Error('not a valid phone number');
+        } else if (Number.isNaN(contact.join(''))) {
+          throw new Error('the value you provided is not a number ');
         }
-    }
-})
+        return true;
+      },
+    },
+  },
+});
 
-function capitalizeFirstChar(str){
-    // text.split(" ") turns strings into an array also considering the space between them (" ") e.g input "damilola jibowu" output ["damilola", "jibowu"] 
-    return str.split(" ").map((ele)=>{
-      return ele.charAt(0).toUpperCase() + ele.slice(1)
-      // join(" ") turns content of an array to single a string having also considering the space in between them (" ")
-    }).join(" ")
+function capitalizeFirstChar(str) {
+  // text.split(" ") turns strings into an array also considering the space between them (" ") e.g input "damilola jibowu" output ["damilola", "jibowu"]
+  return str
+    .split(' ')
+    .map((ele) => ele.charAt(0).toUpperCase() + ele.slice(1) /* join(" ") turns content of an array to single a string having also considering the space in between them (" ") */).join(' ');
 }
 
- phonebookSchema.set('toJSON',{
-    transform:((document,newDocument)=>{
-        newDocument.id = newDocument._id.toString()
-        newDocument.name = capitalizeFirstChar(newDocument.name)
-        delete newDocument._id
-        delete newDocument.__v
-    })
-})
+phonebookSchema.set('toJSON', {
+  transform: (document, newDocument) => {
+    newDocument.id = newDocument._id.toString();
+    newDocument.name = capitalizeFirstChar(newDocument.name);
+    delete newDocument._id;
+    delete newDocument.__v;
+  },
+});
 
-module.exports =  mongoose.model('Person',phonebookSchema)
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = mongoose.model('Person', phonebookSchema);
